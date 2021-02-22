6eaec4b5727bb9a1d668ebfc8eb469ccce724503
Flo <>
2021-02-22 22:28:19 +0100

    Added abstract representation of GameMap consisting of Tiles and TravelConnections

--- Assets/MainGameLoop.cs
+++ Assets/MainGameLoop.cs
@@ -5,9 +5,43 @@ using System.Runtime.InteropServices;
 using UnityEditor.SceneManagement;
 using UnityEngine;
 
+public class TravelConnection
+{
+    public TravelConnection(GameMapTile source, GameMapTile destination, int weight)
+    {
+        Source = source;
+        Destination = Destination;
+        Weight = weight;
+    }
+    
+    /// <summary>
+    /// The cost of movement to travel along this connection to another MapTile 
+    /// </summary>
+    public int Weight { get; set; }
+    /// <summary>
+    /// The GameMapTile this connection originates
+    /// </summary>
+    public GameMapTile Source { get; set; }
+    /// <summary>
+    /// The GameMapTile this connection leads to
+    /// </summary>
+    public GameMapTile Destination { get; set; }
+}
+
+public class GameMapTile
+{
+    /// <summary>
+    /// Holds a GameObject for the visual representation of this GameMapTile
+    /// </summary>
+    public GameObject GameObject { get; set; }
+    
+    public List<TravelConnection> TravelConnections { get; set; }
+}
+
 public class GameMap : MonoBehaviour
 {
-    
+    public GameMapTile MapTiles { get; set; }
+
     void Start()
     {
         
